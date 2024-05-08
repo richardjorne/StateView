7e0536b419744b23c3017bd7258523972d8c3951
richardjorne <154006102+richardjorne@users.noreply.github.com>
2024-05-08 15:37:16 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,13 +10,14 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "StateView",
-            targets: ["SPM/StateView/"]),
+            targets: ["StateView"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "StateView"),
+            name: "StateView",
+            path:  "SPM/StateView/"),
         .testTarget(
             name: "StateViewTests",
             dependencies: ["StateView"]),
